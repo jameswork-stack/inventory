d767b9912fd6fe6a26e74533ad29cb5776016f4e
JAMESKANEKS <kanekikunnie@gmail.com>
2026-01-06 21:28:33 +0800

    update database

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANLKNcrj4JNTyO-xhDY-wVczLUqwe0Ch8",
-  authDomain: "inventory-7bce6.firebaseapp.com",
-  databaseURL: "https://inventory-7bce6-default-rtdb.asia-southeast1.firebasedatabase.app", // 👈 ADD THIS
-  projectId: "inventory-7bce6",
-  storageBucket: "inventory-7bce6.firebasestorage.app",
-  messagingSenderId: "176504896456",
-  appId: "1:176504896456:web:86c1f7b61ac02508135efd",
-  measurementId: "G-WK9VS36NLR"
+  apiKey: "AIzaSyBxZsV0-nxrGnr4xHGDWjBno67tNYzDWo8",
+  authDomain: "paint-inventory-46003.firebaseapp.com",
+  databaseURL: "https://paint-inventory-46003-default-rtdb.asia-southeast1.firebasedatabase.app", // ✅ NEW DB URL
+  projectId: "paint-inventory-46003",
+  storageBucket: "paint-inventory-46003.firebasestorage.app",
+  messagingSenderId: "404843464620",
+  appId: "1:404843464620:web:817c9ef49d70f863d45c09",
+  measurementId: "G-GX2N6RTNT5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Realtime Database
 export const db = getDatabase(app);
+
+// (Optional) Analytics – only works in browser
+export const analytics = getAnalytics(app);
